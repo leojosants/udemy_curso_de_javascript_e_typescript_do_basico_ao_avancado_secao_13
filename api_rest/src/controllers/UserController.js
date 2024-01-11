35b54fb0f390a8636ca71486e421d510dfbcd8d9
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-11 18:10:47 -0300

    Class 179 - Security fixes for users

--- api_rest/src/controllers/UserController.js
+++ api_rest/src/controllers/UserController.js
@@ -3,8 +3,9 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
-      const novoAluno = await User.create(req.body);
-      return res.json(novoAluno);
+      const novoUser = await User.create(req.body);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -15,9 +16,7 @@ class UserController {
   // index
   async index(req, res) {
     try {
-      const users = await User.findAll();
-      console.log('USER ID:', req.userId);
-      console.log('USER EMAIL:', req.userEmail);
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -28,7 +27,8 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -37,19 +37,15 @@ class UserController {
   // update
   async update(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({ errors: ['ID não enviado!'] });
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({ errors: ['USUARIO não existe!'] });
       }
 
       const novosDados = await user.update(req.body);
-
-      return res.json(novosDados);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -60,17 +56,13 @@ class UserController {
   // delete
   async delete(req, res) {
     try {
-      if (!req.params.id) {
-        return res.status(400).json({ errors: ['ID não enviado!'] });
-      }
-
-      const user = await User.findByPk(req.params.id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({ errors: ['USUARIO não existe!'] });
       }
       await user.destroy();
-      return res.json(user);
+      return res.json(null);
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
