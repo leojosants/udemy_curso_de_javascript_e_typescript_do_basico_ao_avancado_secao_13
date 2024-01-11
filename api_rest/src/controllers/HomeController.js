dcd6ec714978588ab142e11f7432862a98fdbd64
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-10 22:02:18 -0300

    Class 174 - Configuring and testing Sequelize

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(request, response) {
-    response.json({
-      tudoCerto: true,
+  async index(request, response) {
+    const novoAluno = await Aluno.create({
+      nome: 'Jose',
+      sobrenome: 'Santos',
+      email: 'jose@gmail.com',
+      idade: 49,
+      peso: 72.0,
+      altura: 1.90,
     });
+    response.json(novoAluno);
   }
 }
 
